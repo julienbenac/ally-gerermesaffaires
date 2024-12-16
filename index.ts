e2fa54fb81bf3018cf36655276fec1db1b1d02ce
Julien Benac <julien.benac@protonmail.com>
2024-12-16 11:30:53 +0100

    fix: add gerermesaffaires token type export

--- index.ts
+++ index.ts
@@ -5,13 +5,17 @@
  * @license MIT
  */
 
+import type { HttpContext } from '@adonisjs/core/http'
+
 export { stubsRoot } from './stubs/main.js'
 export { configure } from './configure.js'
 
-import type { HttpContext } from '@adonisjs/core/http'
-import type { GererMesAffairesDriverConfig } from './src/types/main.js'
+import type { GererMesAffairesDriverConfig, GererMesAffairesToken } from './src/types/main.js'
+
 import { GererMesAffairesDriver } from './src/gerermesaffaires.js'
 
 export function gerermesaffaires(config: GererMesAffairesDriverConfig) {
   return (ctx: HttpContext) => new GererMesAffairesDriver(ctx, config)
 }
+
+export type { GererMesAffairesToken }
